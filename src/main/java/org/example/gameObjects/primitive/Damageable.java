6849e01d3f4690d6d0b6daddee5220957a123c01
MasCode256 <discordeg@mail.ru>
2025-04-18 12:06:17 +0300

    хлобысь 4

--- src/main/java/org/example/gameObjects/primitive/Damageable.java
+++ src/main/java/org/example/gameObjects/primitive/Damageable.java
@@ -3,17 +3,31 @@ package org.example.gameObjects.primitive;
 import org.example.MyGdxGame;
 
 public class Damageable extends ObjectWithCoordinates {
-    protected float hp;
+    protected float hp = 1;
+
+    public Damageable(MyGdxGame game) {
+        super(game);
+    }
 
     public Damageable(MyGdxGame game, float hp) {
         super(game);
         this.hp = hp;
     }
 
+    public Damageable(MyGdxGame game, float x, float y) {
+        super(game, x, y);
+    }
+
     public Damageable(MyGdxGame game, float hp, float x, float y) {
         super(game, x, y);
         this.hp = hp;
     }
+
+    public Damageable(MyGdxGame game, float hp, float x, float y, float angle) {
+        super(game, x, y, angle);
+        this.hp = hp;
+    }
+
 
     public void damage(float damage_points) {
         this.hp -= damage_points;
--- src/main/java/org/example/gameObjects/primitive/ObjectWithCoordinates.java
+++ src/main/java/org/example/gameObjects/primitive/ObjectWithCoordinates.java
@@ -1,11 +1,13 @@
 package org.example.gameObjects.primitive;
 
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 import org.example.MyGdxGame;
 
 import com.badlogic.gdx.math.Rectangle;
 
 public class ObjectWithCoordinates extends GameObject {
-    protected float x = 0f, y = 0f;
+    protected float x = 0f, y = 0f, angle = 0f;
 
     public ObjectWithCoordinates(MyGdxGame game) {
         super(game);
@@ -18,32 +20,49 @@ public class ObjectWithCoordinates extends GameObject {
         this.y = y;
     }
 
+    public ObjectWithCoordinates(MyGdxGame game, float x, float y, float angle) {
+        super(game);
+
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+    }
+
     public void setX(float x) {
         this.x = x;
     }
+    public void setY(float y) { this.y = y; }
+    public void setAngle(float angle) { this.angle = angle; }
 
-    public void setY(float y) {
-        this.y = y;
+    public void setPosition(Vector2 vector) {
+        this.x = vector.x;
+        this.y = vector.y;
     }
+
 
     public float getX() {
         return x;
     }
-
     public float getY() {
         return y;
     }
+    public float getAngle() { return angle; }
 
     public void addX(float a) {
         this.x += a;
     }
-
     public void addY(float a) {
         this.y += a;
     }
+    public void addAngle(float a) { this.angle += a; }
 
     protected void updateCoordinates(Rectangle bounds) {
         bounds.x = this.x;
         bounds.y = this.y;
+    }
+
+    protected void updateCoordinates(Polygon bounds) {
+        bounds.setPosition(this.x, this.y);
+        bounds.setRotation(this.angle);
     }
 }
